4392e7c3f71cbda1702a0513b7e5727eb1c5272e
MohnishKJ <mohnishkj@gmail.com>
2026-07-06 14:48:01 +0530

    feat: add negative login and logout tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,3 +10,9 @@ class LoginPage:
         self.page.fill("#username", username)
         self.page.fill("#password", password)
         self.page.click("#submit")
+
+    def get_error_message(self):
+        return self.page.locator("#error").text_content()
+    
+    def logout(self):
+        self.page.get_by_role("link", name="Log out").click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -13,3 +13,58 @@ def test_valid_login(page):
     )
 
     assert "logged-in-successfully" in page.url
+
+
+def test_invalid_username(page):
+
+    login_page = LoginPage(page)
+
+    login_page.open()
+
+    login_page.login(
+        "wronguser",
+        "Password123"
+    )
+
+    assert login_page.get_error_message() == "Your username is invalid!"
+
+def test_empty_credentials(page):
+
+    login_page = LoginPage(page)
+
+    login_page.open()
+
+    login_page.login(
+        "",
+        ""
+    )
+
+    assert login_page.get_error_message() == "Your username is invalid!"
+
+def test_invalid_password(page):
+
+    login_page = LoginPage(page)
+
+    login_page.open()
+
+    login_page.login(
+        "student",
+        "WrongPassword"
+    )
+
+    assert login_page.get_error_message() == "Your password is invalid!"
+
+def test_logout(page):
+
+    login_page = LoginPage(page)
+
+    login_page.open()
+
+    login_page.login(
+        "student",
+        "Password123"
+    )
+
+    login_page.logout()
+
+    assert "practice-test-login" in page.url
